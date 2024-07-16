8f5eacc822025f08c7bb842ffecdf5bb5511cb74
Maghetto666 <lordrey3@gmail.com>
2024-07-16 17:44:29 +0200

    aggiunti campi password, conferma password, data di nascita, numero di telefono (punto 1)

--- src/main/java/it/giuseppe/form/model/Utente.java
+++ src/main/java/it/giuseppe/form/model/Utente.java
@@ -3,9 +3,15 @@ package it.giuseppe.form.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 @AllArgsConstructor
 public class Utente {
     private String nome;
     private String email;
+    private String password;
+    private String confermaPassword;
+    private String dataDiNascita;
+    private long numeroDiTelefono;
 }
