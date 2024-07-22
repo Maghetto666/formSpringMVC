0990c02e94b2596039c27d66ae504b50f580c094
Maghetto666 <lordrey3@gmail.com>
2024-07-22 16:01:10 +0200

    aggiunta validazione th

--- src/main/java/it/giuseppe/form/controller/UtenteController.java
+++ src/main/java/it/giuseppe/form/controller/UtenteController.java
@@ -1,8 +1,10 @@
 package it.giuseppe.form.controller;
 
 import it.giuseppe.form.model.Utente;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,19 +15,22 @@ import java.util.Objects;
 public class UtenteController {
 
     @GetMapping("/form")
-    public String getForm() {
+    public String getForm(@ModelAttribute Utente utente) {
         return "form";
     }
 
     @PostMapping("/submitForm")
-    public String postForm(@ModelAttribute Utente utente, Model model) {
+    public String postForm(@Valid @ModelAttribute Utente utente, BindingResult res, Model model) {
         if ((utente.getNome().equalsIgnoreCase("admin")) || (utente.getNome().equalsIgnoreCase("root"))) {
             model.addAttribute("error", "Il nome inserito non può essere utilizzato.");
             return "error";
         }
-        if (utente.getNome().length() < 2 || utente.getNome().length() > 20) {
-            model.addAttribute("error", "Il nome inserito deve avere una lunghezza compresa tra i 2 ed i 20 caratteri.");
-            return "error";
+//        if (utente.getNome().length() < 2 || utente.getNome().length() > 20) {
+//            model.addAttribute("error", "Il nome inserito deve avere una lunghezza compresa tra i 2 ed i 20 caratteri.");
+//            return "error";
+//        }
+        if (res.hasErrors()) {
+            return "form";
         }
 
         for (int i = 0; i < utente.getNumeroDiTelefono().length(); i++) {
--- src/main/java/it/giuseppe/form/model/Utente.java
+++ src/main/java/it/giuseppe/form/model/Utente.java
@@ -1,5 +1,8 @@
 package it.giuseppe.form.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -8,8 +11,15 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 public class Utente {
+
+    @NotEmpty(message="Il nome non può essere vuoto")
+    @Size(min = 2, max = 30, message="Il nome deve essere compreso tra i 2 e i 30 caratteri")
     private String nome;
+
+    @NotEmpty(message = "L'email non può essere vuota")
+    @Email(message = "Devi inserire un'email")
     private String email;
+
     private String password;
     private String confermaPassword;
     private String dataDiNascita;
